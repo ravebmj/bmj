9a99ec377359043cb5470e0ea560b51136d04e6a
atif ibrar <abc@gamil.com>
2015-12-18 16:02:50 +0530

    Advance Search Method

--- src/main/java/org/bmj/userinsights/search/controller/SearchResults.java
+++ src/main/java/org/bmj/userinsights/search/controller/SearchResults.java
@@ -1,15 +1,34 @@
 package org.bmj.userinsights.search.controller;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 
 @Controller
 public class SearchResults {
-	
+	/*
+	 * Return Search Result Page
+	 */
     @RequestMapping("/searchresults")  
     public ModelAndView showDashboard() {  
-        return new ModelAndView("searchresults");  
+        ModelAndView model = new ModelAndView("searchresults");  
+        return model;
     } 
+    
+    /*
+     * Advance Search
+     * with Parameter
+     */
+    @RequestMapping(value= "/advanceSearch", method=RequestMethod.POST)  
+    public ModelAndView shows(HttpServletRequest request) {
+    	
+    	ModelAndView model = new ModelAndView("searchresults");  
+        return model; 
+    }    
+    
+    
 }
