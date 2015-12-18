8ba57f2cf7859702d4610e04de26ce6485b7f5ad
Rama Mohan <rama.mohan@rave-tech.co.in>
2015-12-18 17:23:17 +0530

    js folder commit

---
+++ src/main/webapp/js/http _ui.jgrayweb.com_scripts_cal_jq.js
@@ -0,0 +1,16 @@
+function set_up_calendar ()
+{
+	//convention:       <script type="text/javascript">set_up_calendar()</script>
+
+	document.write ('',
+	
+	'<link type="text/css" href="/scripts/jq/themes/base/ui.all.css" rel="stylesheet" />',
+	'<script type="text/javascript" src="/scripts/jq/js/jquery.js"></script>',
+	'<script type="text/javascript" src="/scripts/jq/ui/ui.core.js"></script>',
+	'<script type="text/javascript" src="/scripts/jq/ui/ui.datepicker.js"></script>',
+	'<script type="text/javascript" src="/scripts/jq/demos/datepicker/js/datepicker.js"></script>',
+	'<link type="text/css" href="/scripts/jq/demos/demos.css" rel="stylesheet" />',
+
+	''
+	);
+}
