cb7b25a223d7b97b9a492a6a57fd77cb6f7c8a5a
Rama Mohan <rama.mohan@rave-tech.co.in>
2015-12-22 12:29:59 +0530

    search Dao package and classes creation

---
+++ src/main/java/org/bmj/userinsights/search/Dao/ISearchDao.java
@@ -0,0 +1,5 @@
+package org.bmj.userinsights.search.Dao;
+
+public interface ISearchDao {
+
+}
---
+++ src/main/java/org/bmj/userinsights/search/Dao/SearchDao.java
@@ -0,0 +1,7 @@
+package org.bmj.userinsights.search.Dao;
+
+import org.springframework.orm.hibernate3.support.HibernateDaoSupport;
+
+public class SearchDao extends HibernateDaoSupport implements ISearchDao{
+
+}
