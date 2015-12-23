7e27d5303f31a413b6f7e8efcf397bbaff64959e
Rama Mohan <rama.mohan@rave-tech.co.in>
2015-12-23 20:01:31 +0530

    common method for dropdowns

--- src/main/java/org/bmj/userinsights/dao/UserInsightDaoImpl.java
+++ src/main/java/org/bmj/userinsights/dao/UserInsightDaoImpl.java
@@ -1,8 +1,11 @@
 package org.bmj.userinsights.dao;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import org.bmj.userinsights.common.dto.DecodedNamesDto;
+import org.bmj.userinsights.entity.CodelistCodeDecode;
+import org.bmj.userinsights.entity.CodelistName;
 import org.bmj.userinsights.entity.Person;
 import org.springframework.orm.hibernate4.support.HibernateDaoSupport;
 
@@ -27,14 +30,34 @@ public class UserInsightDaoImpl extends HibernateDaoSupport  implements IUserIns
 	@Override
 	public List<DecodedNamesDto> getCodeListDecodedNames(String codelistName,
 			String applicationId) throws Exception {
+		List<DecodedNamesDto> DecodedNamesDtoLst = new ArrayList<DecodedNamesDto>();
+		CodelistName codelistNameObj = null;
 		
 		List returnVal = this.getHibernateTemplate()
-				.findByNamedQueryAndNamedParam("Person.getPerson",
-						new String[]{""},, new Integer(id));
+				.findByNamedQueryAndNamedParam("CodelistName.getCodelistName",
+						new String[]{"codelistName","applicationId"},new Object[]{codelistName,Integer.valueOf(applicationId)});
+		
+		if(returnVal!=null && returnVal.size()>0){
+			codelistNameObj = (CodelistName) returnVal.get(0);
+		}
+		
+		if(codelistNameObj!=null){
+			List<CodelistCodeDecode> lstCodelistCodeDecode = (List<CodelistCodeDecode>) this.getHibernateTemplate()
+					.findByNamedQueryAndNamedParam("CodelistCodeDecode.getCodelistCodeDecode",
+							new String[]{"codelistId"},new Object[]{codelistNameObj.getCodelistId()});	
+			if(lstCodelistCodeDecode!=null && lstCodelistCodeDecode.size()>0){
+				
+				for(CodelistCodeDecode codeDecode : lstCodelistCodeDecode){
+					DecodedNamesDto dtoObj = new DecodedNamesDto();
+					dtoObj.setCodeDecodedCode(codeDecode.getCodeDecodeCode());
+					dtoObj.setCodeDecodedName(codeDecode.getCodeDecodeDecode());
+					DecodedNamesDtoLst.add(dtoObj);
+				}
+			}
+		}
 		
 		
-		
-		return null;
+		return DecodedNamesDtoLst;
 	}
 
 }
