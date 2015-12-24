1628d8410686ac79f2215f2b95f059626f0056ae
Rama Mohan <rama.mohan@rave-tech.co.in>
2015-12-24 09:00:01 +0530

    setting the dropdownlist to search criteria

--- src/main/java/org/bmj/userinsights/search/dto/SearchCriteria.java
+++ src/main/java/org/bmj/userinsights/search/dto/SearchCriteria.java
@@ -3,33 +3,28 @@ package org.bmj.userinsights.search.dto;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.bmj.userinsights.dashboard.dto.DateCriteriaDto;
 import org.bmj.userinsights.dashboard.dto.InsightTypesDto;
+import org.bmj.userinsights.dashboard.dto.SeveritiesDto;
 
 public class SearchCriteria {
 	
-	private String Keyword;
-	private List<String> severity = new ArrayList<>();
+	private String keyword;
+	private String insightType;
+	private String serverity;
+	private String createdDate;
+	private String fromDate;
+	private String toDate;
+	//private List<String> severity = new ArrayList<>();
 	//private List<String> insightType = new ArrayList<>();
 	List<InsightTypesDto> lstInsightTypesDto = new ArrayList<InsightTypesDto>();
+	List<SeveritiesDto> lstSeveritiesDto = new ArrayList<SeveritiesDto>();
+	List<DateCriteriaDto> lstDateCriteriaDto = new ArrayList<DateCriteriaDto>();
 	private String type;
 	
 	public SearchCriteria(){}
 
-	public String getKeyword() {
-		return Keyword;
-	}
-
-	public void setKeyword(String keyword) {
-		Keyword = keyword;
-	}
-
-	public List<String> getSeverity() {
-		return severity;
-	}
-
-	public void setSeverity(List<String> severity) {
-		this.severity = severity;
-	}
+	
 
 	
 
@@ -48,7 +43,117 @@ public class SearchCriteria {
 	public void setLstInsightTypesDto(List<InsightTypesDto> lstInsightTypesDto) {
 		this.lstInsightTypesDto = lstInsightTypesDto;
 	}
-	
+
+	public String getInsightType() {
+		return insightType;
+	}
+
+	public void setInsightType(String insightType) {
+		this.insightType = insightType;
+	}
+
+
+
+	public String getKeyword() {
+		return keyword;
+	}
+
+
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+
+
+	public List<SeveritiesDto> getLstSeveritiesDto() {
+		return lstSeveritiesDto;
+	}
+
+
+
+	public void setLstSeveritiesDto(List<SeveritiesDto> lstSeveritiesDto) {
+		this.lstSeveritiesDto = lstSeveritiesDto;
+	}
+
+
+
+	public String getServerity() {
+		return serverity;
+	}
+
+
+
+	public void setServerity(String serverity) {
+		this.serverity = serverity;
+	}
+
+
+
+
+
+	public List<DateCriteriaDto> getLstDateCriteriaDto() {
+		return lstDateCriteriaDto;
+	}
+
+
+
+
+
+	public void setLstDateCriteriaDto(List<DateCriteriaDto> lstDateCriteriaDto) {
+		this.lstDateCriteriaDto = lstDateCriteriaDto;
+	}
+
+
+
+
+
+	public String getCreatedDate() {
+		return createdDate;
+	}
+
+
+
+
+
+	public void setCreatedDate(String createdDate) {
+		this.createdDate = createdDate;
+	}
+
+
+
+
+
+	public String getFromDate() {
+		return fromDate;
+	}
+
+
+
+
+
+	public void setFromDate(String fromDate) {
+		this.fromDate = fromDate;
+	}
+
+
+
+
+
+	public String getToDate() {
+		return toDate;
+	}
+
+
+
+
+
+	public void setToDate(String toDate) {
+		this.toDate = toDate;
+	}
+
+
+
 	
 	
 }
