7e5a37eea7e255d9f0bfca2b1c85652e3250045e
unknown <venkateswara.p@rav-lap-194.rave-tech.co.in>
2015-12-27 07:21:56 +0530

    New Entity and hbm files are added.

---
+++ src/main/java/org/bmj/userinsights/entity/MainUserType.java
@@ -0,0 +1,134 @@
+package org.bmj.userinsights.entity;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class MainUserType  implements Serializable{
+	
+	private Integer id;
+	private String name;
+	private String description;
+	private Integer applicationId;
+	private Date addedDate;
+	private Date modifiedDate;
+	private Integer addedUser;
+	private Integer modifiedUser;
+	
+	public MainUserType(){}
+
+	/**
+	 * @return the id
+	 */
+	public Integer getId() {
+		return id;
+	}
+
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(Integer id) {
+		this.id = id;
+	}
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the description
+	 */
+	public String getDescription() {
+		return description;
+	}
+
+	/**
+	 * @param description the description to set
+	 */
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	/**
+	 * @return the applicationId
+	 */
+	public Integer getApplicationId() {
+		return applicationId;
+	}
+
+	/**
+	 * @param applicationId the applicationId to set
+	 */
+	public void setApplicationId(Integer applicationId) {
+		this.applicationId = applicationId;
+	}
+
+	/**
+	 * @return the addedDate
+	 */
+	public Date getAddedDate() {
+		return addedDate;
+	}
+
+	/**
+	 * @param addedDate the addedDate to set
+	 */
+	public void setAddedDate(Date addedDate) {
+		this.addedDate = addedDate;
+	}
+
+	/**
+	 * @return the modifiedDate
+	 */
+	public Date getModifiedDate() {
+		return modifiedDate;
+	}
+
+	/**
+	 * @param modifiedDate the modifiedDate to set
+	 */
+	public void setModifiedDate(Date modifiedDate) {
+		this.modifiedDate = modifiedDate;
+	}
+
+	/**
+	 * @return the addedUser
+	 */
+	public Integer getAddedUser() {
+		return addedUser;
+	}
+
+	/**
+	 * @param addedUser the addedUser to set
+	 */
+	public void setAddedUser(Integer addedUser) {
+		this.addedUser = addedUser;
+	}
+
+	/**
+	 * @return the modifiedUser
+	 */
+	public Integer getModifiedUser() {
+		return modifiedUser;
+	}
+
+	/**
+	 * @param modifiedUser the modifiedUser to set
+	 */
+	public void setModifiedUser(Integer modifiedUser) {
+		this.modifiedUser = modifiedUser;
+	}
+
+	
+	
+
+}
